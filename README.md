088a02beeec76579efd5af871322a0620bcc7146
linweij <1220528736@qq.com>
2019-02-14 07:28:07 -0800

    add crontab task

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# python_celery
-python celery learning
+**简单介绍**
+    Celery 是一个异步任务队列。一个Celery安装有三个核心组件：
+    
+    Celery 客户端: 用于发布后台作业。当与 Flask 一起工作的时候，客户端与 Flask 应用一起运行。
+    
+    Celery workers: 运行后台作业的进程。Celery 支持本地和远程的 workers，可以在 Flask 服务器上启动一个单独的 worker，也可以在远程服务器上启动worker，需要拷贝代码；
+    
+    消息代理: 客户端通过消息队列和 workers 进行通信，Celery 支持多种方式来实现这些队列。最常用的代理就是 RabbitMQ 和 Redis。
+    
--- celery_app/celery_config.py
+++ celery_app/celery_config.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+
+from celery.schedules import crontab
+
 BROKER_URL = 'redis://localhost:6379/1'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/2'
 CELERY_TIMEZONE = 'Asia/Shanghai'
@@ -7,3 +11,17 @@ CELERY_IMPORTS = (
     'celery_app.task1',
     'celery_app.task2',
 )
+
+CELERYBEAT_SCHEDULE = {
+    'task1': {
+        'task': 'celery_app.task.add',
+        'schedule': timedelta(seconds=10),
+        'args': (2, 8)
+    },
+
+    'task2': {
+        'task': 'celery_app.task.multiply',
+        'schedule': crontab(hour=23, minute=22),
+        'args': (3, 7)
+    }
+}
